ec8e25e5fb4ed02ecc053a675af2ffcb903cf229
fedejimenez <fedej_mdp@hotmail.com>
2018-11-25 23:39:07 -0300

    Add autocomplete search

--- react-coin/src/components/common/Search.js
+++ react-coin/src/components/common/Search.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Loading from './Loading';
+import { API_URL } from '../../config';
+import { handleResponse } from '../../helpers';
 import './Search.css';
 
 class Search extends React.Component {
@@ -6,26 +10,107 @@ class Search extends React.Component {
 		super();
 
 		this.state = {
+			searchResults: [],
 			searchQuery: '',
+			loading: false,
 		};
 
 		this.handleChange = this.handleChange.bind(this);
+		this.handleRedirect = this.handleRedirect.bind(this);
 	}
 
 	handleChange(event) {
-		const inputValue = event.target.value;
+		const searchQuery = event.target.value;
 
-		this.setState({ searchQuery: inputValue });	
+		this.setState({ searchQuery });	
 
+		// If search query is empty dont send request
+		if (!searchQuery) {
+			return '';
+		}
+
+		this.setState({ loading: true });
+
+		fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`)
+			.then(handleResponse)
+			.then((result) => {
+				this.setState({ 
+					loading: false,
+					searchResults: result,
+				});
+			});
+	}
+
+	handleRedirect(currencyId) {
+		// Clear input value and close autocomplete container
+		// By clearing searchQuery state
+		this.setState({
+			searchQuery: '',
+			searchResults: [],
+		});
+
+		this.props.history.push(`/currency/${currencyId}`);
+	}
+
+	renderSearchResults() {
+		const { searchResults, searchQuery, loading } = this.state;
+
+		if (!searchQuery){
+			return '';
+		}
+
+		if (searchResults.length > 0) {
+			return (
+				<div className="Search-result-container">
+					{searchResults.map(result => (
+						<div 
+							key={result.id}
+							className="Search-result"
+							onClick={() => this.handleRedirect(result.id)}
+						>
+							{result.name} ({result.symbol})
+						</div>
+					))}
+				</div>
+			);
+		}
+
+		if (!loading) {
+			return (
+				<div className="Search-result-container">
+					<div className="Search-no-result">
+						No results found.
+					</div>
+				</div>
+			);
+		}
 	}
 
 	render() {
+		const { loading, searchQuery } = this.state;
+
 		return (
-			<form>
-				<input onChange={this.handleChange} />
-			</form>
+			<div className="Search">
+				<span className="Search-icon"></span>
+				<input
+					className="Search-input"
+					type="text"
+					placeholder="Currency name" 
+					onChange={this.handleChange}
+					value={searchQuery} 
+				/>
+
+				{loading &&
+					<div className="Search-loading">
+						<Loading 
+							width="12px"
+							height="12px"
+						/>
+					</div>}
+				{this.renderSearchResults()}
+			</div>
 		);
 	}
 }
 
-export default Search;
+export default withRouter(Search);
--- react-coin/src/components/detail/Detail.js
+++ react-coin/src/components/detail/Detail.js
@@ -16,25 +16,39 @@ class Detail extends React.Component {
 	}
 
 	componentDidMount() {
-		const currencyID = this.props.match.params.id;
+		const currencyId = this.props.match.params.id;
 		
+		this.fetchCurrency(currencyId);
+
+	}
+
+	componentWillReceiveProps(nextProps){
+		if (this.props.location.pathname !== nextProps.location.pathname) {
+			// Get new currency id from url
+			const newCurrencyId = nextProps.match.params.id;
+			
+			this.fetchCurrency(newCurrencyId);
+		}
+	}
+
+	fetchCurrency(currencyId){
 		this.setState({ loading: true });
 
-		fetch(`${API_URL}/cryptocurrencies/${currencyID}`)
-			.then(handleResponse)
-			.then((currency) => {
-				this.setState({
-					loading: false, 
-					error: null, 
-					currency, 
-				})
+		fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
+		.then(handleResponse)
+		.then((currency) => {
+			this.setState({
+				loading: false, 
+				error: null, 
+				currency, 
 			})
-			.catch((error) => {
-				this.setState({
-					loading: false, 
-					error: error.errorMessage,
-				})
-			});
+		})
+		.catch((error) => {
+			this.setState({
+				loading: false, 
+				error: error.errorMessage,
+			})
+		});
 	}
 
 	render() {
